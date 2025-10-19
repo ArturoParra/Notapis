1289768b3af20a194863b6210606b79035372911
Arturo <aparrar1900@alumno.ipn.mx>
2025-10-19 12:56:59 -0600

    fix: URL de cliente pwa en WebConfig

--- mi-api-rest/mi-api-rest/src/main/java/com/example/mi_api_rest/config/WebConfig.java
+++ mi-api-rest/mi-api-rest/src/main/java/com/example/mi_api_rest/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Aplica la configuración a todas las rutas bajo /api
-                .allowedOrigins("http://localhost:5173") // Permite tu cliente Vite
+                .allowedOrigins("https://notapispwa.onrender.com") // Permite a URL del sitio donde se hostea el cliente
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*") // Permite todas las cabeceras
                 .allowCredentials(true);
