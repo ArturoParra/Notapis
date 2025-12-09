6fc723fe73f7ef798b3bdda0bfeca50bcd50d4af
Arturo <parra.rivas.arturogabriel@gmail.com>
2025-12-09 07:41:06 -0600

    URL de env en archivo vite.config

--- mi-api-pwa/mi-api-pwa/vite.config.ts
+++ mi-api-pwa/mi-api-pwa/vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa' // <- Importa el plugin
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const apiUrl = (env.VITE_API_URL || 'http://localhost:8080').replace(/\/$/, '');
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
     }),
     tailwindcss(),
     VitePWA({
@@ -51,7 +53,7 @@ export default defineConfig({
           {
             // Regex para interceptar las llamadas a tu API
             // Asegúrate que coincida con la URL de tu API
-            urlPattern: new RegExp('https://notapisbackendserver-ezebehd9erckfqgv.canadacentral-01.azurewebsites.net/api/notas.*'), 
+            urlPattern: new RegExp(`^${apiUrl}/api/notas.*`), 
             
             // Estrategia: StaleWhileRevalidate
             handler: 'StaleWhileRevalidate',
@@ -69,4 +71,8 @@ export default defineConfig({
       }
     })
   ],
+    define: {
+      'process.env': env
+    }
+  }
 })
