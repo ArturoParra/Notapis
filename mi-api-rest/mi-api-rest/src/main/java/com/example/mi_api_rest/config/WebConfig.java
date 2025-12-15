2fc950f71eaab84a56d99c0c00cea54d138894b6
Arturo <parra.rivas.arturogabriel@gmail.com>
2025-12-14 20:14:18 -0600

    feat: configuracion de URLs para GCP

--- mi-api-rest/mi-api-rest/src/main/java/com/example/mi_api_rest/config/WebConfig.java
+++ mi-api-rest/mi-api-rest/src/main/java/com/example/mi_api_rest/config/WebConfig.java
@@ -4,21 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     // 1. Inyectamos el valor desde application.properties (o variable de entorno)
     @Value("${cors.allowed-origins}")
-    private String allowedOrigins;
+    private List<String> allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // 2. Convertimos el texto (separado por comas) en una lista de URLs
-        // Si la variable viene vacía por error, evitamos que falle usando un array vacío
-        String[] origins = allowedOrigins != null ? allowedOrigins.split(",") : new String[]{};
 
         registry.addMapping("/**")
-                .allowedOrigins(origins)
+                .allowedOrigins(allowedOrigins.toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
